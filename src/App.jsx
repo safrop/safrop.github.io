2fc8b344846cb015e08ed401c8b59b932cab5cc9
mac <mac@zk524.com>
2023-08-04 02:27:26 +0800

    action

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,17 @@
-import { useEffect, useState } from 'react';
+import Store, { observer } from './store'
 import { HashRouter as Router, Navigate, useRoutes, Outlet } from 'react-router-dom';
-import { Dialog, DialogContent, DialogTitle, IconButton, Box, Button, TextField, Grid, Stack } from "@mui/material";
-import { Podcasts, Block, Upload, Download } from '@mui/icons-material';
-import { observer } from './store'
-
-const url = new URL("https://safrop.zk524gg.workers.dev")
-// const url = new URL("http://localhost:8787")
-const buffer2hex = (b) => Array.from(new Uint8Array(b)).map((b) => b.toString(16).padStart(2, '0')).join('')
-const hex2buffer = (s) => new Uint8Array(s.match(/../g).map(h => parseInt(h, 16))).buffer
-const sha256buffer = (data) => crypto.subtle.digest('SHA-256', new TextEncoder('utf8').encode(data))
-const sha256 = (data) => sha256buffer(data).then(buffer2hex)
-const enc = (pub, data) => crypto.subtle.encrypt({ name: "RSA-OAEP" }, pub, new TextEncoder().encode(data))
-const dec = (pri, data) => crypto.subtle.decrypt({ name: "RSA-OAEP" }, pri, data).then((data) => new TextDecoder().decode(data))
-const genPub = (n) => crypto.subtle.importKey('jwk', { "alg": "RSA-OAEP-256", "e": "AQAB", "ext": true, "key_ops": ["encrypt"], "kty": "RSA", n }, { name: "RSA-OAEP", hash: "SHA-256" }, true, ["encrypt"])
-const genKey = () => crypto.subtle.generateKey({ name: "RSA-OAEP", modulusLength: 4096, publicExponent: new Uint8Array([1, 0, 1]), hash: "SHA-256" }, true, ["encrypt", "decrypt"])
-  .then(async ({ privateKey, publicKey }) => ([privateKey, (await crypto.subtle.exportKey('jwk', publicKey))['n']]))
+import { Dialog, DialogContent, DialogTitle, IconButton, Box, Button, TextField, Stack } from "@mui/material";
+import { Podcasts, Block } from '@mui/icons-material';
 
 const App = observer(() => {
-  const [ws, WS] = useState({})
-  const [k, K] = useState('')
-  const [v, V] = useState('')
-  const [m, M] = useState([void 0, '', ''])
-  const [u, U] = useState([void 0, ''])
-  const [connected, setConnected] = useState(false)
-
-  const websocket = async () => {
-    url.protocol = "wss"
-    url.pathname = "ws"
-    const ws = new WebSocket(url)
-    ws.addEventListener("open", () => setConnected(true) || sha256(m[1]).then(k => ws.send(k)))
-    ws.addEventListener("close", () => setConnected(false))
-    ws.addEventListener("message", ({ data }) => dec(m[0], hex2buffer(data)).then(V))
-    WS(ws)
-  }
-  useEffect(() => { genKey().then(async ([pri, pub]) => M([pri, pub, await sha256(pub)])) }, [])
-
+  const { K, V, M, U, connected } = Store
   return useRoutes([{
     path: '/', element:
       <Dialog open fullWidth>
         <DialogTitle display='flex' flexWrap="wrap" sx={{ pb: 0, px: 2 }}>
           {connected ?
-            <IconButton onClick={() => ws.close()} children={<Block color='primary' />} /> :
-            <IconButton onClick={() => websocket()} children={<Podcasts color='primary' />} />}
+            <IconButton onClick={() => Store.conn(0)} children={<Block color='primary' />} /> :
+            <IconButton onClick={() => Store.conn(1)} children={<Podcasts color='primary' />} />}
         </DialogTitle>
         <DialogContent>
           <Box sx={{ display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center', width: '100%', aspectRatio: '1/1' }} children={<Outlet />} />
@@ -50,71 +20,23 @@ const App = observer(() => {
     children: [
       {
         path: '', element: <Stack direction={'column'} width={'100%'} height={'100%'} spacing={2} mt={2}>
-          <TextField label='k' value={k} onChange={({ target }) => K(target.value)} />
-          <TextField label='v' rows={6} multiline value={v} onChange={({ target }) => V(target.value)} />
-
+          <TextField label='K' value={K} onChange={({ target }) => Store.set('K', target.value)} />
+          <TextField label='V' value={V} onChange={({ target }) => Store.set('V', target.value)} rows={6} multiline />
           <Stack>
-            <span style={{ fontSize: "12px" }}>M: {m[2]}</span>
-            <span style={{ fontSize: "12px" }}>U: {u[1]}</span>
+            <span>M: {M[2].slice(0, 16)}</span>
+            <span>U: {U[1].slice(0, 16)}</span>
           </Stack>
-
           <Stack spacing={2} direction={'row'}>
-            <Button fullWidth children={'Upload Key'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "up"
-              fetch(url, { method: 'POST', body: JSON.stringify({ k, v: m[1] }) })
-                .then(async () => alert(`UPLOADED: ${await sha256(m[1])}`))
-                .catch(e => console.log(e))
-            }} />
-            <Button fullWidth children={'Download Key'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "dn"
-              fetch(url, { method: 'POST', body: JSON.stringify({ k }) })
-                .then(async (data) => {
-                  const pub = await data.text()
-                  U([await genPub(pub), await sha256(pub)])
-                  alert(`DOWNLOADED: ${await sha256(pub)}`)
-                }).catch(e => console.log(e))
-            }} />
+            <Button fullWidth children={'Upload Key'} variant='contained' onClick={() => Store.up_pub(K, M[1]).then(() => alert(`UPLOADED: ${M[2]}`))} />
+            <Button fullWidth children={'Download Key'} variant='contained' onClick={() => Store.dn_pub(K).then(Store.set_pubkey)} />
           </Stack>
-
           <Stack spacing={2} direction={'row'}>
-            <Button fullWidth children={'Upload Public'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "up"
-              fetch(url, { method: 'POST', body: JSON.stringify({ k, v }) })
-                .then(() => alert(`UPLOADED: ${k}=${v}`))
-                .catch(e => console.log(e))
-            }} />
-            <Button fullWidth children={'Download Public'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "dn"
-              fetch(url, { method: 'POST', body: JSON.stringify({ k }) })
-                .then(d => d.text())
-                .then(V)
-                .catch(e => console.log(e))
-            }} />
+            <Button fullWidth children={'Upload Public'} variant='contained' onClick={() => Store.up_pub(K, V).then(() => alert(`UPLOADED: ${K}=${V}`))} />
+            <Button fullWidth children={'Download Public'} variant='contained' onClick={() => Store.dn_pub(K).then((_) => Store.set('V', _))} />
           </Stack>
-
           <Stack spacing={2} direction={'row'}>
-            <Button fullWidth children={'Upload Secret'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "up"
-              enc(u[0], v)
-                .then(v => fetch(url, { method: 'POST', body: JSON.stringify({ k: u[1], v: buffer2hex(v) }) }))
-                .then(() => console.log(`Sent to ${u[1]}`))
-                .catch(e => console.log(e))
-            }} />
-            <Button fullWidth children={'Download Secret'} variant='contained' onClick={() => {
-              url.protocol = "https"
-              url.pathname = "dn"
-              sha256(m[1])
-                .then(k => fetch(url, { method: 'POST', body: JSON.stringify({ k }) }))
-                .then(d => d.text())
-                .then(d => dec(m[0], hex2buffer(d)))
-                .then(V)
-                .catch(e => console.log(e))
-            }} />
+            <Button fullWidth children={'Upload Secret'} variant='contained' onClick={() => Store.up_sec(V).then(() => alert(`Sent to ${U[1]}`))} />
+            <Button fullWidth children={'Download Secret'} variant='contained' onClick={() => Store.dn_sec()} />
           </Stack>
         </Stack>
       },
@@ -123,5 +45,4 @@ const App = observer(() => {
   { path: '*', element: <Navigate to="/" /> },
   ])
 })
-
 export default () => <Router children={<App />} />
--- src/store.js
+++ src/store.js
@@ -1,9 +1,55 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 export { observer } from 'mobx-react-lite';
 
+const buffer2hex = (b) => Array.from(new Uint8Array(b)).map((b) => b.toString(16).padStart(2, '0')).join('')
+const hex2buffer = (s) => new Uint8Array(s.match(/../g).map(h => parseInt(h, 16))).buffer
+const sha256buffer = (data) => crypto.subtle.digest('SHA-256', new TextEncoder('utf8').encode(data))
+const sha256 = (data) => sha256buffer(data).then(buffer2hex)
+const enc = (pub, data) => crypto.subtle.encrypt({ name: "RSA-OAEP" }, pub, new TextEncoder().encode(data))
+const dec = (pri, data) => crypto.subtle.decrypt({ name: "RSA-OAEP" }, pri, data).then((data) => new TextDecoder().decode(data))
+const genPub = (n) => crypto.subtle.importKey('jwk', { "alg": "RSA-OAEP-256", "e": "AQAB", "ext": true, "key_ops": ["encrypt"], "kty": "RSA", n }, { name: "RSA-OAEP", hash: "SHA-256" }, true, ["encrypt"])
+const genKey = () => crypto.subtle.generateKey({ name: "RSA-OAEP", modulusLength: 4096, publicExponent: new Uint8Array([1, 0, 1]), hash: "SHA-256" }, true, ["encrypt", "decrypt"])
+	.then(async ({ privateKey, publicKey }) => ([privateKey, (await crypto.subtle.exportKey('jwk', publicKey))['n']]))
+
+// const url = new URL("https://safrop.zk524gg.workers.dev")
+const url = new URL("http://localhost:8787")
+const ssl = false
+const r = async (pathname, data) => {
+	[url.protocol, url.pathname] = [ssl ? 'https' : 'http', pathname]
+	return fetch(url, { method: 'POST', body: JSON.stringify(data) }).then(_ => _.text())
+}
+
+
+
 class Store {
+	K = ''
+	V = ''
+	M = [void 0, '', '']
+	U = [void 0, '']
+	W = void 0
+	connected = false
 	constructor() {
 		makeAutoObservable(this);
+		genKey().then(([pri, pub]) => runInAction(async () => (this.M = [pri, pub, await sha256(pub)])))
 	}
+	set = (k, v) => runInAction(() => (this[k] = v))
+	set_pubkey = async (_) => this.set('U', [await genPub(_), await sha256(_)])
+	conn = (ok) => {
+		if (ok) runInAction(() => {
+			[url.protocol, url.pathname] = [ssl ? 'wss' : 'ws', 'ws']
+			this.W = new WebSocket(url)
+			this.W.addEventListener("open", () => this.set('connected', true) && this.W.send(this.M[2]))
+			this.W.addEventListener("close", () => this.set('connected', false))
+			this.W.addEventListener("message", ({ data }) => dec(this.M[0], hex2buffer(data)).then(_ => this.set('V', _)))
+		})
+		else {
+			this.W.close()
+			this.set('connected', false)
+		}
+	}
+	up_pub = (k, v) => r('up', { k, v })
+	dn_pub = (k) => r('dn', { k })
+	up_sec = (v) => enc(this.U[0], v).then(buffer2hex).then((v) => r('up', { k: this.U[1], v }))
+	dn_sec = () => r('dn', { k: this.M[2] }).then(_ => dec(this.M[0], hex2buffer(_))).then(_ => this.set('V', _))
 }
 export default new Store();
