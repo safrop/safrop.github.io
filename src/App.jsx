98272d298fd0d3a712559621a5a63261e6b3c770
mac <mac@zk524.com>
2023-08-05 14:42:52 +0800

    auto connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,93 @@
-import Store, { observer } from './store'
+import { useEffect } from "react";
 import { HashRouter as Router, Navigate, useRoutes, Outlet } from 'react-router-dom';
-import { Dialog, DialogContent, DialogTitle, IconButton, Box, Button, TextField, Stack } from "@mui/material";
-import { Podcasts, Block, Upload, Download, QrCodeScanner } from '@mui/icons-material';
+import { Dialog, DialogContent, DialogTitle, Input, IconButton, Box, ClickAwayListener, MenuList, MenuItem, Popover } from "@mui/material";
+import { Send, QrCodeScanner, Share, Cameraswitch, ContentCopy, Download, Upload, KeyboardArrowLeft, KeyboardArrowRight, Camera, ContentPaste, HighlightOff, HourglassTop } from '@mui/icons-material';
+import store, { observer } from './store'
+
+const path = window.location.pathname;
+['/scan'].includes(path) && (window.location.href = '/#' + path)
+navigator.serviceWorker.addEventListener("message", ({ data }) => store.upload(data.files));
+
+const Scan = () => {
+    useEffect(store.scan, [])
+    return <Box><video style={{ width: '100%' }} /></Box>
+}
+
+const QR = observer(() => {
+    const { qrcode, qrindex, qrsrc } = store
+    return qrsrc
+        ? <><img style={{ width: '100%' }} src={qrsrc} />
+            {qrcode.length > 1 && <Box display='flex' alignItems='center'>
+                <IconButton disabled={qrindex < 1} children={<KeyboardArrowLeft color={qrindex < 1 ? "disabled" : "primary"} fontSize='large' />} onClick={() => store.setIndex(qrindex - 1)} />
+                {qrindex + 1} / {qrcode.length}
+                <IconButton disabled={qrindex + 2 > qrcode.length} children={<KeyboardArrowRight color={qrindex + 2 > qrcode.length ? "disabled" : "primary"} fontSize='large' />} onClick={() => store.setIndex(qrindex + 1)} />
+            </Box>}</>
+        : <QrCodeScanner color='disabled' />
+})
+
+const ShareList = observer(() => <>
+    <IconButton onClick={(e) => store.toggleShare(e.currentTarget)} children={<Share color='primary' />} />
+    <Popover open={Boolean(store.shareAnchor)} anchorEl={store.shareAnchor} onClose={() => store.toggleShare()} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} children={
+        <ClickAwayListener onClickAway={() => store.toggleShare()} children={
+            <MenuList autoFocusItem children={['text', 'file', 'files'].map((type) => <MenuItem key={'share' + type} onClick={() => store.share(type)} children={type} />)} />} />} />
+</>)
+
+const Drop = observer(() => {
+    const { input, key1, key2 } = store
+    return <><IconButton onClick={(e) => store.toggleDrop(e.currentTarget)} children={<Send color='primary' />} />
+        <Popover open={Boolean(store.dropAnchor)} anchorEl={store.dropAnchor} onClose={() => store.toggleDrop()} anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }} children={
+            <ClickAwayListener onClickAway={() => store.toggleDrop()} children={
+                <MenuList autoFocusItem>
+                    <MenuItem key={'up-key'} children={<><span className="material-icons">upload</span><span>My Public Key</span></>} onClick={() =>
+                        store.up_pub(prompt("Input a simple key to upload"), key1[1]).then(() => alert(`Uploaded: ${key1[2].slice(0, 16)}`)).finally(store.toggleDrop)} />
+                    <MenuItem key={'dn-key'} children={<><span className="material-icons">download</span><span>Other side Public Key</span></>} onClick={() =>
+                        store.dn_pub(prompt("Input a given simple key to download other side public key")).then(store.set_key2).then(() => alert(`Downloaded: ${key2[1].slice(0, 16)}`)).finally(store.toggleDrop)} />
+                    <MenuItem key={'up-pub'} children={<><span className="material-icons">upload</span><span>Public Value</span></>} onClick={() =>
+                        store.up_pub(prompt("Input a key to upload your value without encryption"), input).then(() => alert(`Uploaded: ${input}`)).finally(store.toggleDrop)} disabled={!input} />
+                    <MenuItem key={'dn-pub'} children={<><span className="material-icons">download</span><span>Public Value</span></>} onClick={() =>
+                        store.dn_pub(prompt("Input a key to download other side value without encryption")).then(store.setInput).finally(store.toggleDrop)} />
+                    <MenuItem key={'up-sec'} children={<><span className="material-icons">upload</span><span>Secret Value</span></>} onClick={() =>
+                        store.up_sec(store.input).then(() => alert(`Sent to ${key2[1]}`)).finally(store.toggleDrop)} disabled={!key2[1] || !input} />
+                    <MenuItem key={'dn-sec'} children={<><span className="material-icons">download</span><span>Secret Value</span></>} onClick={() =>
+                        store.dn_sec().then(store.setInput).finally(store.toggleDrop)} />
+                    <MenuItem key={'conn-1'} children={<><span className="material-icons">wifi</span><span>Connect From</span></>} onClick={() =>
+                        store.conn().then(() => (store.setInput(key1[1]), store.toggleDrop()))} />
+                    <MenuItem key={'conn-2'} children={<><span className="material-icons">wifi</span><span>Connect To</span></>} onClick={() => {
+                        store.setInput(''), store.toggleDrop(), store.toggleScan()
+                        const timer = setInterval(() => store.scanner || (clearInterval(timer), store.input && store.set_key2(store.input)
+                            .then(() => store.up_pub(store.key2[1], true))
+                            .then(() => store.set({ input: `Connected! ID: ${store.key2[1].slice(0, 32)}` }))), 500)
+                    }} />
+                </MenuList>} />} /></>
+})
 
 const App = observer(() => {
-  const { V, M, U, connected } = Store
-  return useRoutes([{
-    path: '/', element:
-      <Dialog open fullWidth>
-        <DialogTitle display='flex' flexWrap="wrap" sx={{ pb: 0, px: 2 }}>
-          {connected ?
-            <IconButton onClick={() => Store.conn(0)} children={<Block color='primary' />} /> :
-            <IconButton onClick={() => Store.conn(1)} children={<Podcasts color='primary' />} />}
-          <IconButton onClick={() => { }} children={<QrCodeScanner color='primary' />} />
-          <IconButton onClick={() => Store.up_pub(prompt("Upload Your Public Key By A Simple Key:"), M[1]).then(() => alert(`UPLOADED: ${M[2].slice(0, 16)}`))} children={<Upload color='primary' />} />
-          <IconButton onClick={() => Store.dn_pub(prompt("Download Other Side Public Key By A Simple Key:")).then(Store.set_pubkey)} children={<Download color='primary' />} />
-        </DialogTitle>
-        <DialogContent>
-          <Box sx={{ display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center', width: '100%', aspectRatio: '1/1' }} children={<Outlet />} />
-        </DialogContent>
-      </Dialog>,
-    children: [{
-      path: '', element:
-        <Stack direction={'column'} width={'100%'} height={'100%'} spacing={2} mt={2}>
-          <Stack direction={'row'}>
-            <span style={{ width: '100%' }}>M: {M[2].slice(0, 16)}</span>
-            <span style={{ width: '100%' }}>U: {U[1].slice(0, 16) || 'None'}</span>
-          </Stack>
-          <Stack spacing={2} direction={'row'}>
-            <Button fullWidth children={'Up-Pub'} variant='contained' onClick={() => Store.up_pub(prompt("Key:"), V).then(() => alert(`UPLOADED: ${V}`))} />
-            <Button fullWidth children={'Dn-Pub'} variant='contained' onClick={() => Store.dn_pub(prompt("Key:")).then((_) => Store.set('V', _))} />
-            <Button fullWidth children={'Up-Sec'} variant='contained' onClick={() => Store.up_sec(prompt("Value:")).then(() => alert(`Sent to ${U[1]}`))} disabled={!U[1]} />
-            <Button fullWidth children={'Dn-Sec'} variant='contained' onClick={() => Store.dn_sec().then(_ => Store.set('V', _))} />
-          </Stack>
-          <TextField value={V} onChange={({ target }) => Store.set('V', target.value)} multiline minRows={3} />
-        </Stack>
-    }]
-  },
-  { path: '*', element: <Navigate to="/" /> },
-  ])
+    const { input, scanner, loading } = store
+    return useRoutes([{
+        path: '/', element:
+            <Dialog open fullWidth>
+                <DialogTitle display='flex' flexWrap="wrap" sx={{ pb: 0, px: 2 }}>
+                    <IconButton onClick={store.toggleScan} children={<Camera color={scanner ? 'secondary' : 'primary'} />} />
+                    {scanner && <IconButton onClick={store.switchScan} children={<Cameraswitch color='primary' />} />}
+                    <IconButton onClick={store.copy} children={<ContentCopy color='primary' />} />
+                    <IconButton component="label" children={<><Upload color='primary' /><input type="file" multiple accept="image/*" style={{ display: 'none' }} onChange={({ target }) => (store.upload(target.files ?? []), target.value = '')} /></>} />
+                    <IconButton onClick={store.download} children={<Download color='primary' />} />
+                    <ShareList />
+                    <Drop />
+                </DialogTitle>
+                <DialogContent>
+                    <Input endAdornment={<IconButton sx={{ p: 0 }} onClick={input ? store.init : store.paste} children={input ? <HighlightOff /> : <ContentPaste />} />}
+                        fullWidth multiline maxRows={4} sx={{ mb: 2 }} value={input} placeholder={'context'} onChange={(e) => store.setInput(e.target.value)} />
+                    <Box sx={{ display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center', width: '100%', aspectRatio: '1/1' }} children={loading ? <HourglassTop color="primary" /> : <Outlet />} />
+                </DialogContent>
+            </Dialog>,
+        children: [
+            { path: '', element: <QR /> },
+            { path: 'scan', element: <Scan /> },
+        ]
+    },
+    { path: '*', element: <Navigate to="/" /> },
+    ])
 })
+
 export default () => <Router children={<App />} />
--- src/store.js
+++ src/store.js
@@ -1,59 +1,131 @@
-import { makeAutoObservable, runInAction } from 'mobx';
 export { observer } from 'mobx-react-lite';
+import { makeAutoObservable, runInAction } from 'mobx';
+import { BrowserQRCodeReader } from '@zxing/browser';
+import { toDataURL } from 'qrcode';
 
-const CFG = {}
-switch (window.location.host) {
-	case '127.0.0.1':
-	case 'localhost':
-		CFG['url'] = new URL("http://localhost:8787")
-		CFG['ssl'] = false
-	default:
-		CFG['url'] = new URL("https://safrop.zk524gg.workers.dev")
-		CFG['ssl'] = true
+const CFG = { http: new URL('https://safrop.zk524gg.workers.dev'), ws: new URL('wss://safrop.zk524gg.workers.dev/ws') };
+['localhost', '127.0.0.1'].includes(location.hostname) && ([CFG.http, CFG.ws] = [new URL('http://localhost:8787'), new URL('ws://localhost:8787/ws')])
+const request = async (pathname, data) => {
+    CFG.http.pathname = pathname
+    return fetch(CFG.http, { method: 'POST', body: JSON.stringify(data) }).then(_ => _.text())
 }
-
-const r = async (pathname, data) => {
-	[CFG['url'].protocol, CFG['url'].pathname] = [CFG['ssl'] ? 'https' : 'http', pathname]
-	return fetch(CFG['url'], { method: 'POST', body: JSON.stringify(data) }).then(_ => _.text())
-}
-const buffer2hex = (b) => Array.from(new Uint8Array(b)).map((b) => b.toString(16).padStart(2, '0')).join('')
-const hex2buffer = (s) => new Uint8Array(s.match(/../g).map(h => parseInt(h, 16))).buffer
-const sha256buffer = (data) => crypto.subtle.digest('SHA-256', new TextEncoder('utf8').encode(data))
-const sha256 = (data) => sha256buffer(data).then(buffer2hex)
+const buffer2hex = (_) => Array.from(new Uint8Array(_)).map((_) => _.toString(16).padStart(2, '0')).join('')
+const hex2buffer = (_) => new Uint8Array(_.match(/../g).map(h => parseInt(h, 16))).buffer
+const sha256 = (_) => crypto.subtle.digest('SHA-256', new TextEncoder('utf8').encode(_)).then(buffer2hex)
 const enc = (pub, data) => crypto.subtle.encrypt({ name: "RSA-OAEP" }, pub, new TextEncoder().encode(data))
-const dec = (pri, data) => crypto.subtle.decrypt({ name: "RSA-OAEP" }, pri, data).then((data) => new TextDecoder().decode(data))
-const genPub = (n) => crypto.subtle.importKey('jwk', { "alg": "RSA-OAEP-256", "e": "AQAB", "ext": true, "key_ops": ["encrypt"], "kty": "RSA", n }, { name: "RSA-OAEP", hash: "SHA-256" }, true, ["encrypt"])
-const genKey = () => crypto.subtle.generateKey({ name: "RSA-OAEP", modulusLength: 4096, publicExponent: new Uint8Array([1, 0, 1]), hash: "SHA-256" }, true, ["encrypt", "decrypt"])
-	.then(async ({ privateKey, publicKey }) => ([privateKey, (await crypto.subtle.exportKey('jwk', publicKey))['n']]))
+const dec = (pri, data) => crypto.subtle.decrypt({ name: "RSA-OAEP" }, pri, data).then((_) => new TextDecoder().decode(_))
 
 class Store {
-	V = ''
-	M = [void 0, '', '']
-	U = [void 0, '']
-	W = void 0
-	connected = false
-	constructor() {
-		makeAutoObservable(this);
-		genKey().then(([pri, pub]) => runInAction(async () => (this.M = [pri, pub, await sha256(pub)])))
-	}
-	set = (k, v) => runInAction(() => (this[k] = v))
-	set_pubkey = async (_) => this.set('U', [await genPub(_), await sha256(_)])
-	conn = (ok) => {
-		if (ok) runInAction(() => {
-			[CFG['url'].protocol, CFG['url'].pathname] = [CFG['ssl'] ? 'wss' : 'ws', 'ws']
-			this.W = new WebSocket(CFG['url'])
-			this.W.addEventListener("open", () => this.set('connected', true) && this.W.send(this.M[2]))
-			this.W.addEventListener("close", () => this.set('connected', false))
-			this.W.addEventListener("message", ({ data }) => dec(this.M[0], hex2buffer(data)).then(_ => this.set('V', _)))
-		})
-		else {
-			this.W.close()
-			this.set('connected', false)
-		}
-	}
-	up_pub = (k, v) => k ? r('up', { k, v }) : Promise.reject("Empty Key")
-	dn_pub = (k, peek) => k ? r('dn', { k, peek }) : Promise.reject("Empty Key")
-	up_sec = (v) => v ? enc(this.U[0], v).then(buffer2hex).then((v) => r('up', { k: this.U[1], v })) : Promise.reject("Empty Value")
-	dn_sec = () => r('dn', { k: this.M[2] }).then(_ => _ ? dec(this.M[0], hex2buffer(_)) : _)
+    qreader = new BrowserQRCodeReader(new Map().set(2, [11]), { delayBetweenScanAttempts: 100, delayBetweenScanSuccess: 100 })
+    qrindex = 0
+    qrcode = ['']
+    qrdata = ['']
+    qrsrc = ''
+    input = ''
+    shareAnchor = null
+    dropAnchor = null
+    loading = false
+    scanner = false
+    scanmode = 'environment'
+    key1 = [void 0, '', '']
+    key2 = [void 0, '']
+    connected = false
+    constructor() {
+        makeAutoObservable(this);
+        this.set_key1()
+    }
+    set = (o) => {
+        runInAction(() => Object.keys(o).forEach((k) => this[k] = o[k]))
+    }
+    init = (o = {}) => {
+        this.set({ qrindex: 0, qrcode: [''], qrdata: [''], qrsrc: '', input: '', scanner: false, loading: false, ...o })
+    }
+    scan = () => {
+        this.init({ scanner: true })
+        const promise = this.qreader.decodeFromConstraints({ video: { facingMode: this.scanmode, width: 600, height: 600 } }, document.querySelector('video'), (i) => i && (this.setInput(i.getText()), this.toggleScan(0)))
+        return () => { promise.then((s) => s.stop()) }
+    }
+    setInput = (i) => {
+        this.init({ input: i })
+        if (i !== '') {
+            const qrdata = i.match(/[^]{2048}|[^]+/g)
+            const qrcode = qrdata.map((v) => toDataURL(v, { margin: 1, scale: 16 }))
+            Promise.all(qrcode).then((qrcode) => this.set({ qrcode, qrdata, qrsrc: qrcode[0] }))
+        } else this.qrsrc = ''
+    }
+    setIndex = (i) => {
+        this.set({ qrindex: i, qrsrc: this.qrcode[i], input: this.qrdata[i] })
+    }
+    toggleScan = (off) => {
+        if (off === 0 || this.scanner) (this.scanner = false, location.hash = '/')
+        else (this.scanner = true, location.hash = '/scan')
+    }
+    switchScan = () => {
+        Promise.resolve(location.hash = '/')
+            .then(() => this.scanmode = this.scanmode === 'environment' ? 'user' : 'environment')
+            .then(() => location.hash = '/scan')
+    }
+    toggleShare = (anchor = null) => {
+        this.set({ shareAnchor: this.shareAnchor ? null : anchor })
+    }
+    copy = () => {
+        navigator.clipboard.writeText(this.input)
+    }
+    paste = () => {
+        navigator.clipboard.readText().then(this.setInput)
+    }
+    upload = (files) => {
+        this.init()
+        if (this.scanner) this.toggleScan()
+        for (const file of files) {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (e) => Promise.resolve((e.target).result).then((qrsrc) =>
+                this.qreader.decodeFromImageUrl(qrsrc).then((d) => d.getText()).catch(() => 'invalid').then((input) => this.qrcode[0]
+                    ? this.set({ qrcode: [...this.qrcode, qrsrc], qrdata: [...this.qrdata, input], qrsrc: this.qrcode[0], input: this.qrdata[0] })
+                    : this.set({ qrcode: [qrsrc], qrdata: [input], qrsrc, input })))
+        }
+    }
+    download = () => {
+        let a = document.createElement("a")
+        if (this.qrsrc) (a.download = 'qrcode.png', a.href = this.qrsrc, a.click())
+        a = null
+    }
+    __share__ = (src, index = 0) => {
+        const [mime, data] = src.split(",")
+        const bstr = Buffer.from(data, 'base64').toString('latin1')
+        return new File([new Uint8Array(bstr.length).map((_, i) => bstr.charCodeAt(i))], `qrcode${index}.png`, { type: mime.match(/:(.*?);/)?.[1] })
+    }
+    share = (type) => {
+        this.set({ shareAnchor: null })
+        if (!this.qrsrc) return
+        switch (type) {
+            case 'text': return navigator.share({ text: this.input })
+            case 'file': return navigator.share({ files: [this.__share__(this.qrsrc)] })
+            case 'files': return navigator.share({ files: this.qrcode.map(this.__share__) })
+            default: return
+        }
+    }
+
+    set_key1 = () => crypto.subtle.generateKey({ name: "RSA-OAEP", modulusLength: 4096, publicExponent: new Uint8Array([1, 0, 1]), hash: "SHA-256" }, true, ["encrypt", "decrypt"])
+        .then(async ({ privateKey, publicKey }) => ([privateKey, (await crypto.subtle.exportKey('jwk', publicKey))['n']]))
+        .then(async ([pri, pub]) => this.set({ key1: [pri, pub, await sha256(pub)] }))
+    set_key2 = (n) => crypto.subtle.importKey('jwk', { "alg": "RSA-OAEP-256", "e": "AQAB", "ext": true, "key_ops": ["encrypt"], "kty": "RSA", n }, { name: "RSA-OAEP", hash: "SHA-256" }, true, ["encrypt"])
+        .then(_ => this.set({ key2: [_, ''] }))
+        .then(() => sha256(n))
+        .then(_ => this.set({ key2: [this.key2[0], _] }))
+    up_pub = (k, v) => k ? request('up', { k, v }) : Promise.reject("Empty Key")
+    dn_pub = (k, peek) => k ? request('dn', { k, peek }) : Promise.reject("Empty Key")
+    up_sec = (v) => v ? enc(this.key2[0], v).then(buffer2hex).then((v) => request('up', { k: this.key2[1], v })) : Promise.reject("Empty Value")
+    dn_sec = () => request('dn', { k: this.key1[2] }).then(_ => _ ? dec(this.key1[0], hex2buffer(_)) : _)
+    conn = async () => {
+        const ws = new WebSocket(CFG['ws'])
+        ws.addEventListener("open", () => ws.send(this.key1[2]))
+        ws.addEventListener("close", () => this.input.includes('connecting') && this.set({ input: 'Connection timed out!' }))
+        ws.addEventListener("message", ({ data }) => this.set({ input: data === 'true' ? `Connected! ID: ${this.key1[2].slice(0, 32)}` : `connecting...(${data})` }))
+    }
+    toggleDrop = (anchor = null) => {
+        this.set({ dropAnchor: this.dropAnchor ? null : anchor })
+    }
 }
 export default new Store();
