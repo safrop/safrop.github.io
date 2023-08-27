8ef6a336bee13f8f30414dc485fc2f5261f6e7a9
mac <mac@zk524.com>
2023-08-28 06:48:03 +0800

    support file

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const response = (body, status = { status: 200 }) => {
 addEventListener('fetch', (e) => e.respondWith((async () => {
   let params = {}
   switch (new URL(e.request.url).pathname) {
+    case '/up_file':
+      params.data = await e.request.arrayBuffer()
+      params.keylength = new Uint8Array(params.data.slice(0, 1))[0]
+      params.key = params.data.slice(1, params.keylength + 1)
+      params.keyname = new TextDecoder().decode(params.key)
+      await safrop.put(params.keyname, params.data.slice(params.keylength + 1))
+      return response(params.keyname)
+    case '/dn_file':
+      params = await e.request.json()
+      return safrop.get(params['k'], { type: 'arrayBuffer' }).then((v) => {
+        return response(v)
+      })
     case '/up':
       params = await e.request.json()
       if (await safrop.get(params['k']) && params['k'].length < 32) return response('Existed', { status: 401 })
